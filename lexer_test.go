285089516a4797f9a29e1d17d02d189cc6a4e82a
Colton Baker <github@netrefuge.net>
2014-07-28 19:05:20 -0400

    Update tests; add in a hex test for all languages.

--- lexer_test.go
+++ lexer_test.go
@@ -10,7 +10,6 @@ import (
 var l = lexer.New()
 
 func TestParse(t *testing.T) {
-
 	scan, err := l.Parse("Javascript", `/*
 
   Animal can be used as a base for different types of animals.
@@ -62,19 +61,48 @@ module.exports = {
 `)
 
 	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-		return
+		t.Fatalf("Unexpected error: %s", err)
 	}
 
 	Status("Scan: %+v", scan)
+}
+
+func TestParseHex(t *testing.T) {
+	str := "0x00 0xFF 0xFFZZ"
+	expects := [][]string{
+		{"HEX", "0x00"},
+		{"WHITESPACE", " "},
+		{"HEX", "0xFF"},
+		{"WHITESPACE", " "},
+		{"WORD", "0xFFZZ"},
+	}
+
+	for lang := range lexer.Languages {
+		scan, err := l.Parse(lang, str)
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		for i, expect := range expects {
+			if i > len(scan.Scanner.Tokens)-1 {
+				t.Fatalf("Excpected more output: %+v\n", expects[i:])
+			}
+			if scan.Scanner.Tokens[i][0] != expect[0] ||
+				scan.Scanner.Tokens[i][1] != expect[1] {
+				t.Fatalf("Expected %+v, got %+v", expect, scan.Scanner.Tokens[i])
+			}
+		}
+
+		Status("Scan: %+v", scan)
+	}
 }
 
 func TestParseNoDataError(t *testing.T) {
 	_, err := l.Parse()
 
 	if err == nil {
-		t.Errorf("Expected error.")
-		return
+		t.Fatalf("Expected error.")
 	}
 
 	Status("Got error: %+v", err)
@@ -84,8 +112,7 @@ func TestParseStringFirstError(t *testing.T) {
 	_, err := l.Parse([]int{1, 2, 3, 4, 5}, "Test")
 
 	if err == nil {
-		t.Errorf("Expected error.")
-		return
+		t.Fatal("Expected error.")
 	}
 
 	Status("Got error: %+v", err)
@@ -95,8 +122,7 @@ func TestParseScannerError(t *testing.T) {
 	_, err := l.Parse([]int{1, 2, 3, 4, 5})
 
 	if err == nil {
-		t.Errorf("Expected error.")
-		return
+		t.Fatalf("Expected error.")
 	}
 
 	Status("Got error: %+v", err)
@@ -107,8 +133,7 @@ func TestReadFile(t *testing.T) {
 	files, err := ioutil.ReadDir("testdata")
 
 	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-		return
+		t.Fatalf("Unexpected error: %s", err)
 	}
 
 	Status("Scanning all files found in testdata directory")
@@ -119,8 +144,7 @@ func TestReadFile(t *testing.T) {
 		scan, err := l.ReadFile(file)
 
 		if err != nil {
-			t.Errorf("Unexpected error: %s", err)
-			return
+			t.Fatalf("Unexpected error: %s", err)
 		}
 
 		Status("Scanned: %+v", scan)
@@ -132,8 +156,7 @@ func TestReadFileInvalidFileError(t *testing.T) {
 	_, err := l.ReadFile("testdata/idontexist")
 
 	if err == nil {
-		t.Errorf("Expected error.")
-		return
+		t.Fatal("Expected error.")
 	}
 
 	Status("Got error: %+v", err)
